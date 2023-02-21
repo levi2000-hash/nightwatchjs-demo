a4da0c69a56931fa592d9eb0a2e60bc7143b9560
Levi Noeninckx <levi.noeninckx@gmail.com>
2023-02-21 17:04:01 +0100

    quick update

--- nightwatch/examples/basic/ecosia.js
+++ nightwatch/examples/basic/ecosia.js
@@ -1,6 +1,5 @@
 describe('Ecosia.org Demo', function() {
   before(browser => browser.navigateTo('https://www.ecosia.org/'));
-
   it('Demo test ecosia.org', function(browser) {
     browser
       .waitForElementVisible('body')
